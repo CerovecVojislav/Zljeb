197b30a27581e10a571a60e1e0c0d9c53b4c9fa1
CerovecVojislav <woja12@gmail.com>
2023-08-11 15:30:56 +0200

    first commit

---
+++ src/main/java/com/voja/Zljeb/ZljebApplication.java
@@ -0,0 +1,13 @@
+package com.voja.Zljeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZljebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZljebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voja/Zljeb/ZljebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voja.Zljeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZljebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
